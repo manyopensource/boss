3c2ae8eeade4525888d8034439119ced1de13759
kairat <2deserve@gmail.com>
2019-01-14 12:56:00 +0600

    Component for displaying datapicker like other form fields

---
+++ src/features/profile/components/FormFieldDate.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Field } from 'react-final-form';
+import PropTypes from 'prop-types';
+import BossDatePicker from './../../datepicker/components/BossDatePicker';
+import FormErrorMessage from './FormErrorMessage';
+
+class FormFieldDate extends Component {
+  render() {
+    return (
+      <Field
+        name={this.props.name}
+        type="text"
+        render={({ input, meta }) => (
+          <div className="boss-form__field">
+            <p className="boss-form__label">
+              <span className="boss-form__label-text">
+                {this.props.label}
+                {this.props.required ? '*' : null}
+              </span>
+            </p>
+            <div className="date-picker-input date-picker-input_type_icon">
+              <BossDatePicker
+                date={input.value}
+                {...input}
+                onApply={momentObj => input.onChange(momentObj)}
+              />
+            </div>
+            {meta.touched &&
+              meta.error && <FormErrorMessage errorMessage={meta.error} />}
+          </div>
+        )}
+      />
+    );
+  }
+}
+
+FormFieldDate.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  required: PropTypes.bool
+}
+
+export default FormFieldDate;
