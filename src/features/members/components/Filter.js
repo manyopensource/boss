59dd166d203a58fd4cf8f52c4d0677d41bd7cf2b
kairat <2deserve@gmail.com>
2019-01-14 20:12:51 +0600

    Component Filter which able to self-toggle

---
+++ src/features/members/components/Filter.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import FilterForm from './FilterForm';
+
+class Filter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFilterOpened: false
+    };
+  }
+
+  handleToggleFilter = () => {
+    this.setState({
+      isFilterOpened: !this.state.isFilterOpened
+    });
+  };
+
+  render() {
+    return (
+      <div className="boss-showdown">
+        <div className="boss-dropdown__header">
+          <a
+            className={
+              this.state.isFilterOpened
+                ? 'boss-dropdown__switch boss-dropdown__switch_role_filter boss-dropdown__switch_state_opened'
+                : 'boss-dropdown__switch boss-dropdown__switch_role_filter'
+            }
+            onClick={this.handleToggleFilter}
+          >
+            Filter
+          </a>
+        </div>
+        <div
+          className={
+            this.state.isFilterOpened
+              ? 'boss-dropdown__content boss-dropdown__content_state_opened'
+              : 'boss-dropdown__content'
+          }
+          // style={{ display: 'none' }}
+        >
+          <div className="boss-dropdown__content-inner">
+            <FilterForm />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Filter;
