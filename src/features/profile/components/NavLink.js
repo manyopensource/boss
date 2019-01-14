c901f8b9e0d17b4c654fdc40bc420dd2f405a09b
kairat <2deserve@gmail.com>
2019-01-14 12:45:19 +0600

    Implementation of NavLink (from react-router-dom) for displaying navigation links

--- src/features/profile/components/NavLink.js
+++ src/features/profile/components/NavLink.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { NavLink as A } from 'react-router-dom';
 
 class NavLink extends Component {
+  handleClick = e => {
+    this.props.setActiveChapter(this.props.link.chapter);
+  };
+
   render() {
     return (
-      <a
+      <A
+        to={'/' + this.props.link.chapter}
+        activeClassName="boss-content-switcher__nav-link_state_active"
         className="boss-content-switcher__nav-link"
         data-chapter={this.props.link.chapter}
+        onClick={this.handleClick}
       >
         {this.props.link.title}
-      </a>
+      </A>
     );
   }
 }
