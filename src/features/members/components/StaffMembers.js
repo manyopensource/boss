983f6dfe14c0a1f082fe2e0c2a350cfd71551a5c
kairat <2deserve@gmail.com>
2019-01-14 18:56:37 +0600

    New page's component StaffMembers

---
+++ src/features/members/components/StaffMembers.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Dashboard from './Dashboard';
+
+class StaffMembers extends Component {
+  render() {
+    return (
+      <>
+        <Dashboard />
+      </>
+    );
+  }
+}
+
+export default StaffMembers;
