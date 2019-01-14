50053a43b90bcdac3f3cb0eaf3d5efbab9bdddc5
kairat <2deserve@gmail.com>
2019-01-14 12:48:29 +0600

    Component for displaying list of links

--- src/features/profile/components/Nav.js
+++ src/features/profile/components/Nav.js
@@ -3,11 +3,15 @@ import PropTypes from 'prop-types';
 import NavLink from './NavLink';
 
 class Nav extends Component {
+  onSetActiveChapter = chapter => {
+    this.props.setActiveChapter(chapter);
+  }
+
   render() {
     return (
       <nav className="boss-content-switcher__nav">
-        {this.props.links.map(link => {
-          return <NavLink link={link} />;
+        {this.props.links.map((link, index) => {
+          return <NavLink key={index} link={link} activeChapter={this.props.activeChapter} setActiveChapter={this.onSetActiveChapter} />;
         })}
       </nav>
     );
