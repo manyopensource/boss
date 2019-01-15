4a46667db02127ff7a0d50d8c79fc99f8a8e2bc4
kairat <2deserve@gmail.com>
2019-01-15 15:17:05 +0600

    Some content and its rendering after iteration

--- src/features/members/components/StaffMembers.js
+++ src/features/members/components/StaffMembers.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 import Dashboard from './Dashboard';
+import Content from './../../Content';
+import Table from './Table';
+import TableRow from './TableRow';
 
 class StaffMembers extends Component {
   render() {
+    const members = [
+      {
+        id: 1,
+        pic: 'default.jpeg',
+        name: 'John Doe',
+        modifiedAt: '01/15/2019',
+        status: 'Enabled',
+        type: 'Manager',
+        masterVenue: 'Black',
+        workVenues: ['Mint Green', "McCooley's"]
+      },
+      {
+        id: 2,
+        pic: 'default.jpeg',
+        name: 'Brian Smith',
+        modifiedAt: '01/15/2019',
+        status: 'Disabled',
+        type: 'Manager',
+        masterVenue: 'Brooklyn Mixer',
+        workVenues: ['Black', "McCooley's"]
+      }
+    ];
     return (
       <>
         <Dashboard />
+        <Content>
+          <Table>
+            <TableRow isHeader={true} />
+            {members.map(member => {
+              return <TableRow key={member.id} member={member} />;
+            })}
+          </Table>
+        </Content>
       </>
     );
   }
