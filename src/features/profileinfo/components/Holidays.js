67f75dd3e6e213e4b0d69cfd64015f44ba994b1a
kairat <2deserve@gmail.com>
2019-01-16 15:56:26 +0600

    Component Holidays

---
+++ src/features/profileinfo/components/Holidays.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Holidays extends Component {
+  render() {
+    return (
+      <section className="boss-board">
+        <header className="boss-board__header">
+          <h2 className="boss-board__title">Holidays and holiday requests</h2>
+          <div className="boss-board__button-group">
+            <p className="boss-button boss-button_role_add">Add Holiday</p>
+          </div>
+        </header>
+      </section>
+    );
+  }
+}
+
+export default Holidays;
