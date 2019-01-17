64d03697fb6603c502851b79be807e052b01ef35
kairat <2deserve@gmail.com>
2019-01-17 12:11:46 +0600

    Routes has changed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 import EditProfile from './features/profile/components/EditProfile';
 import StaffMembers from './features/members/components/StaffMembers';
 import Profile from './features/profileinfo/components/Profile';
+import NotFound from './features/NotFound';
 
 class App extends Component {
   render() {
     return (
       <main className="boss-page-main">
-        <Route exact path="/edit-profile" render={() => <Redirect to="/edit-profile/employment" />} />
-        <Route exact path="/edit-profile/:page" component={EditProfile} />
-        <Route exact path="/staff-members" component={StaffMembers} />
-        <Route exact path="/profile/:id" component={Profile} />
-        <Route exact path="/profile/:id/holidays" component={Profile} />
-        <Route exact path="/profile/:id/owed-hours" component={Profile} />
-        <Route exact path="/profile/:id/accessories" component={Profile} />
-        <Route exact path="/profile/:id/shifts" component={Profile} />
-        <Route exact path="/profile/:id/payments" component={Profile} />
+        <Switch>
+          <Route
+            exact
+            path="/staff-member/:id/edit"
+            render={props => (
+              <Redirect
+                to={`/staff-member/${props.match.params.id}/edit/employment`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/staff-member/:id/edit/:page"
+            component={EditProfile}
+          />
+          <Route exact path="/staff-members" component={StaffMembers} />
+          <Route exact path="/staff-member/:id" component={Profile} />
+          <Route exact path="/staff-member/:id/:page" component={Profile} />
+          {/* <Redirect exact from="/" to="/staff-members" /> */}
+          <Route path="*" component={NotFound} />
+        </Switch>
       </main>
     );
   }
