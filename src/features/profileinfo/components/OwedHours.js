daf98f62429358f48f57709ec0643efc931b504f
kairat <2deserve@gmail.com>
2019-01-17 11:55:49 +0600

    Component OwedHours

---
+++ src/features/profileinfo/components/OwedHours.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class OwedHours extends Component {
+  render() {
+    return (
+      <section className="boss-board">
+        <header className="boss-board__header">
+          <h2 className="boss-board__title">Owed Hours</h2>
+          <div className="boss-board__button-group">
+            <p className="boss-button boss-button_role_add">Add Hours</p>
+          </div>
+        </header>
+      </section>
+    );
+  }
+}
+
+export default OwedHours;
