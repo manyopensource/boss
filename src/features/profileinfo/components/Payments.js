e7752ac65370f1573255e0c965112af005d0d687
kairat <2deserve@gmail.com>
2019-01-17 11:56:03 +0600

    Component Payments

---
+++ src/features/profileinfo/components/Payments.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Payments extends Component {
+  render() {
+    return (
+      <section className="boss-board">
+        <header className="boss-board__header">
+          <h2 className="boss-board__title">Payments</h2>
+        </header>
+      </section>
+    );
+  }
+}
+
+export default Payments;
