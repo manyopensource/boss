93ab9b834095d9dff600ef456eb37ecb67a02935
kairat <2deserve@gmail.com>
2019-01-17 12:10:21 +0600

    Component NotFound added

---
+++ src/features/NotFound.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Content from './Content';
+
+class Profile extends Component {
+  render() {
+    return <Content>Page not found. Sorry (((</Content>;
+  }
+}
+
+export default Profile;
