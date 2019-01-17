29d9e6ac1bd29d47ff4961d9b863772ac6fd2916
kairat <2deserve@gmail.com>
2019-01-17 11:55:18 +0600

    Component Shifts

---
+++ src/features/profileinfo/components/Shifts.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Shifts extends Component {
+  render() {
+    return (
+      <section className="boss-board">
+        <header className="boss-board__header">
+          <h2 className="boss-board__title">Shifts</h2>
+        </header>
+      </section>
+    );
+  }
+}
+
+export default Shifts;
