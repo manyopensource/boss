bd7ec11456d35fe079816788a3481809102c5dba
kairat <2deserve@gmail.com>
2019-01-18 15:47:34 +0600

    Fetching real data from server-side

--- src/features/profileinfo/components/Profile.js
+++ src/features/profileinfo/components/Profile.js
@@ -2,93 +2,95 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Content from './../../Content';
-import Flow from './Flow';
-import Holidays from './Holidays';
-import OwedHours from './OwedHours';
-import Accessories from './Accessories';
-import Shifts from './Shifts';
-import Payments from './Payments';
+import PageProfile from './PageProfile';
+import PageHolidays from './PageHolidays';
+import PageOwedHours from './PageOwedHours';
+import PageAccessories from './PageAccessories';
+import PageShifts from './PageShifts';
+import PagePayments from './PagePayments';
+import { http } from './../../../utils/HttpService';
 
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      membersData: {
-        id: 1,
-        fullname: 'John Doe',
-        email: 'johndoe@my.com',
-        phone: '+123 342 342',
-        venue: 'Some Venue Name',
-        employmentDetails: {
-          'Main Venue': "McCoolay's",
-          'Other Venues': 'N / A',
-          'Job Type': 'Floor Staff',
-          'Start Date': 'Tue 11/15/2018',
-          'Pay Rate': 'Age 18-20',
-          'Hour Preference': '20 - 25',
-          'Day Preference': ['Thursday', 'Saturday', 'Sunday'],
-          'National Insurance Number': 'PE630024B',
-          'Sage ID': 260,
-          'Status Statement': 'A'
-        },
-        accountDetails: {
-          Created: 'Mon 11/14/2018',
-          Status: 'Active',
-          User: 'No Associated User',
-          'Application Password': '10:30 Mon 11/15/2018',
-          'ID Scanner App Guid': 'qr.png'
-        },
-        personalDetails: {
-          Name: 'John Doe',
-          Gender: 'Female',
-          'Date of Birth': '23 April 1999'
-        },
-        contactDetails: {
-          'Email Address': 'johndoe@my.com',
-          'Phone Number': '+123 342 342',
-          Address: {
-            street: '907 Charisse Junction',
-            city: 'New Jersey',
-            country: 'Monaco',
-            postCode: '42448'
-          }
-        },
-        mobileApps: {
-          'Some App': {
-            link: 'some-app',
-            lastSentDateTime: '10:30 Mon 11/15/2018'
-          },
-          'Other App': {
-            link: 'other-app',
-            lastSentDateTime: '10:30 Mon 11/15/2018'
-          },
-          'Another App': {
-            link: 'another-app',
-            lastSentDateTime: '10:30 Mon 11/15/2018'
-          }
-        },
-        revisionsHistory: {
-          viewHistory: 'view-history'
-        }
-      }
+      isLoaded: false,
+      staffMember: null,
+      staffTypes: null,
+      venues: null,
+      payRates: null,
+      genderValues: null
     };
+  }
+
+  componentDidMount() {
+    http.getStaffMember(this.props.match.params.id).then(result => {
+      this.setState({
+        isLoaded: true,
+        staffMember: result.data.staffMember,
+        staffTypes: result.data.staffTypes,
+        venues: result.data.venues,
+        payRates: result.data.payRates,
+        genderValues: result.data.genderValues
+      });
+    });
   }
 
   render() {
     return (
-      <>
-        <Dashboard membersData={this.state.membersData} memberId={this.props.match.params.id} />
-        <Content>
-          <Switch>
-            <Route exact path="/staff-member/:id" render={() => <Flow membersData={this.state.membersData} />} />
-            <Route exact path="/staff-member/:id/holidays" component={Holidays} />
-            <Route exact path="/staff-member/:id/owed-hours" component={OwedHours} />
-            <Route exact path="/staff-member/:id/accessories" component={Accessories} />
-            <Route exact path="/staff-member/:id/shifts" component={Shifts} />
-            <Route exact path="/staff-member/:id/payments" component={Payments} />
-          </Switch>
-        </Content>
-      </>
+      this.state.isLoaded && (
+        <>
+          <Dashboard
+            staffMember={this.state.staffMember}
+            staffTypes={this.state.staffTypes}
+            venues={this.state.venues}
+            payRates={this.state.payRates}
+            genderValues={this.state.genderValues}
+          />
+          <Content>
+            <Switch>
+              <Route
+                exact
+                path="/staff-member/:id"
+                render={() => (
+                  <PageProfile
+                    staffMember={this.state.staffMember}
+                    staffTypes={this.state.staffTypes}
+                    venues={this.state.venues}
+                    payRates={this.state.payRates}
+                    genderValues={this.state.genderValues}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/staff-member/:id/holidays"
+                component={PageHolidays}
+              />
+              <Route
+                exact
+                path="/staff-member/:id/owed-hours"
+                component={PageOwedHours}
+              />
+              <Route
+                exact
+                path="/staff-member/:id/accessories"
+                component={PageAccessories}
+              />
+              <Route
+                exact
+                path="/staff-member/:id/shifts"
+                component={PageShifts}
+              />
+              <Route
+                exact
+                path="/staff-member/:id/payments"
+                component={PagePayments}
+              />
+            </Switch>
+          </Content>
+        </>
+      )
     );
   }
 }
