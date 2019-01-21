938e5cc5f7be5361a4ee350840b20c8b91e520e0
kairat <2deserve@gmail.com>
2019-01-21 11:43:46 +0600

    Affected by Renaming some components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
-import EditProfile from './features/staffMemberEdit/components/EditProfile';
-import StaffMembers from './features/staffMembers/components/StaffMembers';
-import Profile from './features/staffMember/components/Profile';
+import StaffMembers from './features/staffMembers/containers/StaffMembers';
+import StaffMember from './features/staffMember/containers/StaffMember';
+import StaffMemberEdit from './features/staffMemberEdit/components/StaffMemberEdit';
 import NotFound from './features/NotFound';
 
 class App extends Component {
@@ -24,11 +24,11 @@ class App extends Component {
           <Route
             exact
             path="/staff-member/:id/edit/:page"
-            component={EditProfile}
+            component={StaffMemberEdit}
           />
           <Route exact path="/staff-members" component={StaffMembers} />
-          <Route exact path="/staff-member/:id" component={Profile} />
-          <Route exact path="/staff-member/:id/:page" component={Profile} />
+          <Route exact path="/staff-member/:id" component={StaffMember} />
+          <Route exact path="/staff-member/:id/:page" component={StaffMember} />
           <Redirect exact from="/" to="/staff-members" />
           <Route path="*" component={NotFound} />
         </Switch>
