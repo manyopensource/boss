93e84e65764b3a2494e4b8d8a9e77ff6b5e7ac11
kairat <2deserve@gmail.com>
2019-01-23 12:28:31 +0600

    Routes was corrected

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 import StaffMembers from './features/staffMembers/containers/StaffMembers';
 import StaffMember from './features/staffMember/containers/StaffMember';
-import StaffMemberEdit from './features/staffMemberEdit/components/StaffMemberEdit';
 import NotFound from './features/NotFound';
 
 class App extends Component {
@@ -12,23 +11,8 @@ class App extends Component {
     return (
       <main className="boss-page-main">
         <Switch>
-          <Route
-            exact
-            path="/staff-member/:id/edit"
-            render={props => (
-              <Redirect
-                to={`/staff-member/${props.match.params.id}/edit/employment`}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/staff-member/:id/edit/:page"
-            component={StaffMemberEdit}
-          />
           <Route exact path="/staff-members" component={StaffMembers} />
-          <Route exact path="/staff-member/:id" component={StaffMember} />
-          <Route exact path="/staff-member/:id/:page" component={StaffMember} />
+          <Route strict path="/staff-member/:id" component={StaffMember} />
           <Redirect exact from="/" to="/staff-members" />
           <Route path="*" component={NotFound} />
         </Switch>
