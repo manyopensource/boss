f478fb9c1973bb7afca37d107d2f6c9619ef1bbe
kairat <2deserve@gmail.com>
2019-01-25 14:47:55 +0600

    Added submitError and some legacy code has been removed

--- src/features/staffMemberEdit/components/PollBox.js
+++ src/features/staffMemberEdit/components/PollBox.js
@@ -2,54 +2,62 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Field } from 'react-final-form';
 import FormErrorMessage from './../../FormErrorMessage';
-import FormFieldRadio from './FormFieldRadio';
 
 class PollBox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      checked: false
-    };
-  }
-
-  onChangeCheckedState = bool => {
-    console.log('changeCheckedState');
-    this.setState({
-      checked: bool
-    });
-  };
-
   render() {
     return (
       <Field
         type="radio"
         name={this.props.name}
         options={this.props.options}
-        render={({ input, options, meta }) => (
-          <div className="boss-form__field">
-            <div className="boss-choice-list">
-              <div className="boss-choice-list__header">
-                <h3 className="boss-choice-list__title">
-                  {this.props.boxTitle}
-                </h3>
-              </div>
-              <div className="boss-choice-list__content">
-                <p className="boss-choice-list__text">{this.props.pollTitle}</p>
-                <div className="boss-choice-list__controls">
-                  {options.map((option, index) => (
-                    <FormFieldRadio
-                      key={option.value}
-                      input={input}
-                      option={option}
-                    />
-                  ))}
+        render={({ input, options, meta }) => {
+          return (
+            <div className="boss-form__field">
+              <div className="boss-choice-list">
+                <div className="boss-choice-list__header">
+                  <h3 className="boss-choice-list__title">
+                    {this.props.boxTitle}
+                  </h3>
+                </div>
+                <div className="boss-choice-list__content">
+                  <p className="boss-choice-list__text">
+                    {this.props.pollTitle}
+                  </p>
+                  <div className="boss-choice-list__controls">
+                    {options.map((option, index) => (
+                      <label
+                        key={index}
+                        className="boss-choice-list__radio-label"
+                      >
+                        <input
+                          className="boss-choice-list__radio-button"
+                          type="radio"
+                          ref={input => (this.input = input)}
+                          name={input.name}
+                          value={option.value}
+                          onChange={e => {
+                            input.onChange(option.value);
+                          }}
+                          checked={
+                            option.value === (input.value || meta.initial)
+                          }
+                        />
+                        <span className="boss-choice-list__radio-label-text">
+                          {option.label}
+                        </span>
+                      </label>
+                    ))}
+                  </div>
                 </div>
               </div>
+              {meta.touched && (meta.error || meta.submitError) && (
+                <FormErrorMessage
+                  errorMessage={meta.error || meta.submitError}
+                />
+              )}
             </div>
-            {meta.touched &&
-              meta.error && <FormErrorMessage errorMessage={meta.error} />}
-          </div>
-        )}
+          );
+        }}
       />
     );
   }
