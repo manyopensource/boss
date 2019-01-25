1fdfc3a9df2dcd90ef87bc6cb01f26f6d74aeb6b
kairat <2deserve@gmail.com>
2019-01-25 15:39:39 +0600

    Added package Reselect

--- src/selectors.js
+++ src/selectors.js
@@ -1,6 +1,12 @@
+import { createSelector } from 'reselect';
+
 export const getStaffMembers = state => state.staffMembers;
 export const getStaffMember = state => state.staffMember;
 export const getStaffTypes = state => state.staffTypes;
 export const getVenues = state => state.venues;
 export const getPayRates = state => state.payRates;
 export const getGenderValues = state => state.genderValues;
+
+export const getStaffMembersData = state => {
+
+}
