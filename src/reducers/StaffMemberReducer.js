fbf9de65e8c589ce7985e14514cb2c507a5d7636
kairat <2deserve@gmail.com>
2019-01-25 14:36:06 +0600

    Reducer was corrected

--- src/reducers/StaffMemberReducer.js
+++ src/reducers/StaffMemberReducer.js
@@ -1,7 +1,10 @@
 import {
   LOAD_STAFF_MEMBER_STARTUP,
   LOAD_STAFF_MEMBER_SUCCESS,
-  LOAD_STAFF_MEMBER_FAILURE
+  LOAD_STAFF_MEMBER_FAILURE,
+  UPDATE_STAFF_MEMBER_STARTUP,
+  UPDATE_STAFF_MEMBER_SUCCESS,
+  UPDATE_STAFF_MEMBER_FAILURE
 } from './../constants/ActionTypes';
 
 const reducer = (
@@ -17,6 +20,16 @@ const reducer = (
     case LOAD_STAFF_MEMBER_SUCCESS:
       return { ...state, isLoading: false, data: action.payload.staffMember };
     case LOAD_STAFF_MEMBER_FAILURE:
+      return { ...state, isLoading: false };
+    case UPDATE_STAFF_MEMBER_STARTUP:
+      return { ...state, isLoading: true };
+    case UPDATE_STAFF_MEMBER_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        data: { ...state.data, ...action.payload }
+      };
+    case UPDATE_STAFF_MEMBER_FAILURE:
       return { ...state, isLoading: false };
     default:
       return state;
