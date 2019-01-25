c00682c180f9f2e2ceed393d2ae49b1a846f19dd
kairat <2deserve@gmail.com>
2019-01-25 18:35:33 +0600

    Selectors and submit of data by get-request (url-encoded)

--- src/features/staffMembers/components/FilterForm.js
+++ src/features/staffMembers/components/FilterForm.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import { Form } from 'react-final-form';
 import FormField from './../../FormField';
 import FormFieldSelect from './../../FormFieldSelect';
 import FormFieldSelectCheckbox from './FormFieldSelectCheckbox';
-
-function onSubmit(values) {
-  console.log(values);
-}
 
 class FilterForm extends Component {
   constructor(props) {
@@ -16,6 +13,14 @@ class FilterForm extends Component {
     };
   }
 
+  handleSubmit = values => {
+    console.log(values);
+    this.props.history.push({
+      // pathname: '/search',
+      search: '?' + new URLSearchParams(values).toString()
+    });
+  };
+
   onToggleMasterVenueOnly = bool => {
     this.setState({
       masterVenueOnly: bool
@@ -24,9 +29,8 @@ class FilterForm extends Component {
 
   render() {
     const options = [
-      { value: 'chocolate', label: 'Chocolate' },
-      { value: 'strawberry', label: 'Strawberry' },
-      { value: 'vanilla', label: 'Vanilla' }
+      { value: 'enabled', label: 'Enabled' },
+      { value: 'disabled', label: 'Disabled' }
     ];
     const options2 = [
       { value: 'watermelon', label: 'Watermelon' },
@@ -35,8 +39,8 @@ class FilterForm extends Component {
     ];
     return (
       <Form
-        onSubmit={onSubmit}
-        render={({ handleSubmit, pristine, invalid }) => (
+        onSubmit={this.handleSubmit}
+        render={({ handleSubmit, meta, pristine, invalid }) => (
           <form className="boss-form" onSubmit={handleSubmit}>
             <div className="boss-form__row boss-form__row_position_last">
               <div className="boss-form__group boss-form__group_layout_half">
@@ -52,14 +56,18 @@ class FilterForm extends Component {
                 <FormFieldSelectCheckbox
                   name="venue"
                   label="Venue"
-                  options={this.state.masterVenueOnly ? options2 : options}
+                  options={
+                    this.state.masterVenueOnly
+                      ? options2
+                      : this.props.venuesForSelect
+                  }
                   masterVenueOnly={this.state.masterVenueOnly}
                   toggleMasterVenueOnly={this.onToggleMasterVenueOnly}
                 />
                 <FormFieldSelect
                   name="staffType"
                   label="Staff Type"
-                  options={options}
+                  options={this.props.staffTypesForSelect}
                 />
                 <div className="boss-form__field boss-form__field_justify_end boss-form__field_no-label">
                   <button
@@ -78,4 +86,4 @@ class FilterForm extends Component {
   }
 }
 
-export default FilterForm;
+export default withRouter(FilterForm);
