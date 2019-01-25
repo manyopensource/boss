7d4f10c2ebf9242f24f0e798bbee0cbdcbfc7a51
kairat <2deserve@gmail.com>
2019-01-25 15:10:12 +0600

    All selectors are here

---
+++ src/selectors.js
@@ -0,0 +1,6 @@
+export const getStaffMembers = state => state.staffMembers;
+export const getStaffMember = state => state.staffMember;
+export const getStaffTypes = state => state.staffTypes;
+export const getVenues = state => state.venues;
+export const getPayRates = state => state.payRates;
+export const getGenderValues = state => state.genderValues;
